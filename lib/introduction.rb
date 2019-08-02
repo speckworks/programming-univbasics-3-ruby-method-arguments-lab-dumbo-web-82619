f0195ee453050e55d4d085c1114004cbfedb7ed6
John Karrell Speck <speckworks@gmail.com>
2019-08-02 17:37:38 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+#intro
+def introduction(#{name})
+  puts "Hi, my name is #{name}!"
+end 
+
+#intro with coding language specialty
+def coding_with_language(#{name}, #{language})
+  puts "Hi, my name is #{name} and I am learning to program in #{language}!"
+end
+
+  #introduction with language optional
+  def introduction_with_language_optional(#{name}, #{language})
+    puts "Hi, my name is #{name} and I am learning to code in #{Ruby}!
+end  
